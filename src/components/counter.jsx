89e79546db2e766f9aaa56d99fc774dc8ff972c3
Tanapattara Wongkhamchan <coach.tpr@gmail.com>
2021-08-02 16:29:39 +0700

    create component

---
+++ src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {count: 0, imageUrl: "https://picsum.photos/50",};
+    render() { 
+        let classes = this.getBadgeClasses();
+
+        return (
+            <div>
+                <img className="img-thumbnail" src={this.state.imageUrl} alt="picsum" />
+                <span className={classes}>{this.getCount()}</span>
+                <button
+                    onClick={this.handleIncrement}
+                    className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+            );
+    }
+
+    getCount(){
+        const { count } = this.state;
+        return count === 0 ? <b>Zero</b> : count;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += this.state.count === 0 ? "bg-warning" : "bg-primary";
+        return classes;
+    }
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
+}
+ 
+export default Counter;
